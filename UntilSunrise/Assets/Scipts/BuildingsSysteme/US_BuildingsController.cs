7986d26e2ca4072215d3fca309faa3cca8dda7bd
LoganSanz <118187068+LoganSANZ@users.noreply.github.com>
2024-01-12 10:17:40 +0100

    Buildings Shooting

--- UntilSunrise/Assets/Scipts/BuildingsSysteme/US_BuildingsController.cs
+++ UntilSunrise/Assets/Scipts/BuildingsSysteme/US_BuildingsController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -15,6 +16,8 @@ public class US_BuildingsController : MonoBehaviour
     [SerializeField] private int arrowDamage;
     [SerializeField] private float arrowRange;
     [SerializeField] private float arrowShootingSpeed;
+
+    private bool hasShoot = false;
 
     public GameObject currentEnnemy;
 
@@ -34,7 +37,7 @@ public class US_BuildingsController : MonoBehaviour
         else if (buildingType == GameState.ArrowThrower)
         {
             DetectionRange(arrowRange);
-            //MakeDamage(arrowDamage);
+            //MakeDamage(arrowDamage)
         }
 
         if (currentEnnemy != null)
@@ -48,16 +51,68 @@ public class US_BuildingsController : MonoBehaviour
     {
         Collider[] collision = Physics.OverlapSphere(transform.position, _range, ennemiesLayer);
 
-        foreach (Collider _coll in collision) 
+        foreach (Collider _coll in collision)
         {
             distance = Vector3.Distance(transform.position, _coll.ClosestPoint(transform.position));
             currentEnnemy = _coll.gameObject;
+
+            //Stone Shoot
+            if (buildingType == GameState.StoneThrower)
+            {
+                US_EnemyStats eStats = currentEnnemy.GetComponent<US_EnemyStats>();
+
+                if (!hasShoot && currentEnnemy != null)
+                {
+                    StartCoroutine(StoneShoot(eStats, stoneDamage));
+                    hasShoot = false;
+
+                    if (eStats.eIsDead)
+                    {
+                        currentEnnemy = null;
+                    }
+
+                }
+
+                if (currentEnnemy == null)
+                {
+                    StopCoroutine(StoneShoot(eStats, stoneDamage));
+                }
+
+            }
+            //Arrow Shoot
+            else if (buildingType == GameState.ArrowThrower)
+            {
+                US_EnemyStats eStats = currentEnnemy.GetComponent<US_EnemyStats>();
+
+                if (eStats.eIsDead)
+                {
+                    currentEnnemy = null;
+                }
+
+                if (currentEnnemy == null)
+                {
+                    StopCoroutine(ArrowShoot(eStats, arrowDamage));
+                }
+            }
         }
 
         if (distance > _range)
         {
             currentEnnemy = null;
         }
+    }
+
+    private IEnumerator StoneShoot(US_EnemyStats _eStats, int _damage)
+    {
+        _eStats.TakeDamage(_damage);
+        yield return new WaitForSeconds(stoneShootingSpeed);
+        hasShoot = true;
+    }
+    private IEnumerator ArrowShoot(US_EnemyStats _eStats, int _damage)
+    {
+        _eStats.TakeDamage(_damage);
+        yield return new WaitForSeconds(arrowShootingSpeed);
+        hasShoot = true;
     }
 
     public enum GameState
